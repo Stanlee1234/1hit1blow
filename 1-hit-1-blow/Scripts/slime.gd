1e395507c9013f48a1993181833caa80d3ec15e7
Stanlee1234 <lustanleyz@gmail.com>
2025-12-07 12:31:40 -0800

    started slime

---
+++ 1-hit-1-blow/Scripts/slime.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+
+
+
+func _on_detect_area_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_detect_area_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
